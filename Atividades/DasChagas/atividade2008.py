295a424b042796e83181daa3fe9cbc0339d2b122
das <francisconunesdcfmec@gmail.com>
2024-08-20 19:38:11 -0300

    Adicionando minha atividade do dia 20/08

---
+++ Atividades/DasChagas/atividade2008.py
@@ -0,0 +1,7 @@
+"""Olá, essa é minha atividade do dia 20/08 do curso de introdução a programação. Esse programa faz algumas perguntas básicas sobre quem está acessando ele, é um programa simples e funcional!"""
+nome = str(input('Olá, qual o seu nome?'))
+idade = int(input('Qual a sua idade?'))
+altura = bool(input('Qual a sua altura? (em metros, ex: 1.80)'))
+sorte = int(input('Qual o seu número da sorte?'))
+#resposta para duas perguntas que foram feitas anteriormente.
+print (' Olá {}, que bom que está testando esse programa, vi que você tem o número {} como número da sorte, que legal!'.format(nome, sorte))
