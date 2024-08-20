d6f9c4aa05e015655e342c4e85cf5115ea6c49cb
marcelospfcufc <marcelospfcufc@gmail.com>
2024-08-20 20:20:45 -0300

    adicionando a solução da atividade 1.

---
+++ Atividades/Solucoes/Atividade1/at1.py
@@ -0,0 +1,38 @@
+variavel_str = "variavel 1"
+variavel_int = 10
+variavel_float = 10.1
+
+print(type(variavel_str))
+print(type(variavel_int))
+print(type(variavel_float))
+
+# 2 questão
+''' 2 questão '''
+""" 2 questão """
+
+''' Questão 3
+Crie um comentário de 
+mais de uma linha.
+'''
+
+""" Questão 3 ...
+Crie um comentário de 
+mais de uma linha.
+"""
+
+# 4 Questão
+nome = input("Entre com o seu nome: ")
+
+# 5 questão
+
+numero = input("Entre com um número: ")
+print("O numero digitado foi", numero)
+
+print(type(numero)) # obrigatoriamente é str
+
+# 6 Questão
+
+numero1 = int(input("Digite o primeiro numero: "))
+numero2 = int(input("Digite o segundo numero: "))
+print(numero1 + numero2)
+print(numero1 * numero2)
