676b474d88883771a155750c1e8987868db86095
das <francisconunesdcfmec@gmail.com>
2024-08-20 20:05:00 -0300

    Corrigindo alguns erros na atividade

--- Atividades/DasChagas/atividade2008.py
+++ Atividades/DasChagas/atividade2008.py
@@ -1,7 +1,12 @@
-"""Olá, essa é minha atividade do dia 20/08 do curso de introdução a programação. Esse programa faz algumas perguntas básicas sobre quem está acessando ele, é um programa simples e funcional!"""
+"""Olá, essa é minha atividade do dia 20/08 do curso de introdução a programação. 
+Esse programa faz algumas perguntas básicas sobre quem está acessando ele, é um programa simples e funcional!"""
 nome = str(input('Olá, qual o seu nome?'))
 idade = int(input('Qual a sua idade?'))
-altura = bool(input('Qual a sua altura? (em metros, ex: 1.80)'))
+altura = float(input('Qual a sua altura? (em metros, ex: 1.80)'))
 sorte = int(input('Qual o seu número da sorte?'))
 #resposta para duas perguntas que foram feitas anteriormente.
 print (' Olá {}, que bom que está testando esse programa, vi que você tem o número {} como número da sorte, que legal!'.format(nome, sorte))
+print (type(nome))
+print (type(idade))
+print (type(altura))
+print (type(sorte))
