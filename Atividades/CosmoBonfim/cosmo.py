afd58b0db12de8a39d8a1303150494fd17c55f8b
CosmoBonf <cosmoaraujo525@gmail.com>
2024-08-21 00:47:20 +0000

    modificado codigo da aula

--- Atividades/CosmoBonfim/cosmo.py
+++ Atividades/CosmoBonfim/cosmo.py
@@ -1,1 +1,26 @@
+numero = 5
 
+numero += 7
+print(numero) 
+
+var = 5 
+print(var == 5)
+
+print(var != 7)
+
+print(var > 2)
+
+print(var >= 5)
+
+print(var < 7)
+
+print(not True)
+
+num1 = 7
+num2 = 4 
+
+print(num1 > 3 and num2 < 8)
+
+print(num1 > 4 or num2 <= 8)
+
+print(not (num1 < 30 and num2 < 8))
