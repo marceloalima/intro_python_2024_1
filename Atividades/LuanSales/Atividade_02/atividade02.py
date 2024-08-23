b5cda3a05023f1a7e117a05fa7f353a9cb70d4e7
Luan Dev <luansales.oficial@gmail.com>
2024-08-22 21:09:18 -0300

    added atividade 02

--- Atividades/LuanSales/Atividade_02/atividade02.py
+++ Atividades/LuanSales/Atividade_02/atividade02.py
@@ -11,6 +11,8 @@ n1 = int(input('Digite um número: '))
 n2 = int(input('Digite outro número: '))
 if n1 > n2:
     print(f'O maior número é {n1}')
+else:
+    print(f'O maior número é {n2}')
 
 # Q.03
 '''Faça um algoritmo para calcular a média aritmética M entre duas notas de um aluno e mostrar sua situação, que pode ser: 
