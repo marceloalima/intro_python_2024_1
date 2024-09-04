51ccaa66bf9c6f2baf0abc4e16120bf832f47453
LuanDev <165568493+LuanSalesx@users.noreply.github.com>
2024-09-03 21:38:17 -0300

    Update questao04.py

--- Atividades/LuanSales/execicio_Lista/questao04.py
+++ Atividades/LuanSales/execicio_Lista/questao04.py
@@ -1,12 +1,4 @@
-numero1 = 1
-# Números de 1 a 10
-while numero1 <= 10:
-    numero2 = 1
-    print(f"=> Tabuada do {numero1}:")
-
-    # While para realizar a multiplicação
-    while numero2 <= 10:
-        resultado = numero1 * numero2
-        print(f"{numero1} x {numero2} = {resultado}")
-        numero2 += 1
-    numero1 += 1
+for i in range(1, 11):
+    for j in range(1, 11):
+        print(f"{i} x {j} = {i*j}")
+    print("-" * 12)
