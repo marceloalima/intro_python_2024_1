96d1d8b99fd644a8f01cc2e9c200859e0cb8991e
ValescaYorrana <valescayorrana@gmail.com>
2024-10-08 19:44:39 -0300

    Atividades feitas no dia 30-09

---
+++ Atividades/Valesca/Exercicio-de-sala4-Q1-v1-simplificada.py
@@ -0,0 +1,29 @@
+# Essa foi a que eu fiz ( tentativa )
+
+mat = [ 8 , 7 , 9 ]
+fis = [ 6 , 5 , 7 ]
+qui = [ 10 , 9 , 8 ]
+
+notas = mat, fis, qui
+
+print(f"as notas do aluno foram em, {"matematica"} : {mat}.")
+print(f"as notas do aluno foram em, {"fisica"} : {fis}.")
+print(f"as notas do aluno foram em, {"quimica"} : {qui}.")
+
+media_mat = [ ( 8 + 7 + 9 ) / 3 ]
+
+media_fis = [ ( 6 + 5 + 7 ) / 3 ]
+
+media_qui = [ ( 10 + 9 + 8 ) / 3]
+
+print("as medias das notas são : ",media_mat,media_fis,media_qui)
+
+soma = [ 8 + 6 + 9 ] 
+print("a soma das notas é : ",soma)
+
+media = [ 23 / 3 ]
+
+print(f"a media do aluno é : {media}.")
+
+''' não gostei muito dessa versão porque ficou simples demais.
+    queira ter feito com o "for", mas não desenrolei muito na hora '-' '''
